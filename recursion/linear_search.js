a4156534ccfcb77cb925ee0bc2b0c2e2450a9e7a
Aman Roy <royaman56456@gmail.com>
2024-04-06 23:00:45 +0530

    Committing linear search code

--- recursion/linear_search.js
+++ recursion/linear_search.js
@@ -18,7 +18,12 @@ function linearSearch(arr, target, index = 0) {
 console.log(linearSearch([1, 4, 6, 7, 8, 45, 78], 7)); // Output: 3
 
 
-// Recurrence Relation
+// Time Complexity-> O(n), where n is the number of elements in the array. In the worst-case scenario, linear search may need to iterate through all 
+//n elements to find the target.
+// Space Complexity->O(1). Linear search doesn't use additional space proportional to the size of the input. The space used remains constant regardless of the size of the input array.
 
-// Time Complexity
-// Space Complexity
+
+
+
+
+
