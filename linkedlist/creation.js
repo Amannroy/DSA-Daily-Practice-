be0c5c429bc5b2202edfb7e25e8f20f0e2f653c9
Aman Roy <royaman56456@gmail.com>
2024-04-18 07:40:08 +0530

    Committing linkedlist  creation code

--- linkedlist/creation.js
+++ linkedlist/creation.js
@@ -1,4 +1,4 @@
-// How to create class Node
+//1. How to create class Node
 
 // In this ES6 version, the Node class is defined with a constructor that initializes data and next. The setNext and getNext methods are defined directly within the class body, making the code more readable and concise.
 
@@ -13,7 +13,7 @@ class Node{
     constructor(data){
         // Assigning the passed data to the data property of the Node object
     this.data = data;
-    // Initiaize the next property to null, indicatingthe end of the list
+    // Initialize the next property to null, indicatingthe end of the list
     this.next = null;
 }
 
@@ -29,3 +29,89 @@ getNext(){
     return this.next;
 }
 }
+
+
+//2. Linked List class that will manage the nodes. This class will have methods to add nodes, remove nodes, and display the list
+
+// Defining the linkedlist class
+class LinkedList{
+    // Constructor function to initialize a new linkedlist object
+    constructor(){
+        // Initializing the head to the list to null, indicating an empty list
+        this.head = null;
+        // Initializing the size of the list to 0
+        this.size = 0;
+    }
+    // Method to add an element at the end of the list
+    add(element){
+        // Create a new Node with the given element
+           let node = new Node(element);
+           // Declaring a variable to hold the current node during traversal
+           let current;
+           // Checking if the list is empty(head is null)
+           if(this.head === null){
+            // If empty, set the head tot he new node
+            this.head = node;
+           }else{
+            // If not empty, traverse to the end of the list
+            current = this.head;
+            while(current.next){
+                current = current.next;
+            }
+            // Linking the last node to the new node
+            current.next = node;
+           }
+           // Incrementing the size of the list
+           this.size++;
+    }
+
+    //Method to insert an element at a specific position
+    insertAt(element,index){
+        // Checking if the index is valid
+        if(index < 0 || index > this.size){
+            // If not, log an error message
+            return console.log("Please enter a valid index,");
+        }
+        // Creating a new Node with the given element
+        let node = new Node(element);
+        // Declaring variables to hold the current and previous nodes during traversal
+        let curr, prev;
+        // Starting the traversal from the head
+        curr = this.head;
+        // Checking if inserting at the beginning of the list
+        if(index === 0){
+            // If so, setting the new node's next to the current head(This ensures new node points to the current head)
+            node.next = this.head;
+            // Updating the head to the new node(This makes the new node the new head of the list)
+            this.head = node;
+        }else{
+            // If not, traverse to the desired position
+            let it = 0;// Initializing an iterator to keep track of the current position
+            while(it < index){
+                // Continuing traversing until reaching the desired index
+                it++; // Incrementing the iterator to move closer to the desired index
+                prev = curr; // Updating the previous node reference to the current node before moving to the next node
+                curr = curr.next; // Moving to the next node in the linked list
+            }
+            // Linking the new node to the new node
+            node.next = curr; // Linking the new node to the current node by setting the next pointer t the current node's next
+
+            // Linking the previous node to the new node
+            prev.next = node; // updating the next pointer of the previous node to point to the new node, effectively inserting the new node into the linked list
+        }
+
+
+
+    }
+
+
+
+    removeFrom(index){
+
+
+    }
+}
+
+
+
+
