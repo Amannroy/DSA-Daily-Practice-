838fcb0614e16a3640647da434e88a13bd0498f9
Aman Roy <86224475+Amannroy@users.noreply.github.com>
2024-06-25 19:03:22 +0530

    Update insertionSort.js

--- sorting/insertionSort.js
+++ sorting/insertionSort.js
@@ -1,23 +1,30 @@
 // Insertion Sort function
-function insertionSort(arr) {
-  const n = arr.length;
-
-  for (let i = 1; i < n; i++) {
-    // n
-    const key = arr[i];
-    let j = i - 1;
-    while (j >= 0 && arr[j] > key) {
-      // n
-      arr[j + 1] = arr[j];
-      j--;
+// Insertion Sort
+function insertionSort(nums) {
+    // Iterate over the array starting from the second element (index 1)
+    for (let i = 1; i < nums.length; i++) {
+        // Store the current element as the key to be inserted
+        let key = nums[i];
+        // Initialize j to the index before i
+        let j = i - 1;
+        // Move elements of nums[0..i-1], that are greater than key, to one position ahead
+        // of their current position
+        while (j >= 0 && nums[j] > key) {
+            // Shift the element at index j to index j+1
+            nums[j + 1] = nums[j];
+            // Decrement j to compare key with the next element on the left
+            j--;
+        }
+        // Place the key at its correct position in the sorted subarray
+        nums[j + 1] = key;
     }
-    arr[j + 1] = key;
-  }
-
-  return arr;
+    // Return the sorted array
+    return nums;
 }
 
+// Test the insertionSort function with the given array and print the result
 console.log(insertionSort([29, 10, 14, 37, 14, 33, 8, 11]));
+
 
 // Best Case Time Complexity = O(n)
 // Worst Case Time Complexity = O(n^2)
